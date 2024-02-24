ea5dfc778228c6eb72f82b95847669c464513a56
Hemanth-bonala-11 <hemanth.bonala11@gmail.com>
2024-02-25 02:40:00 +0530

    profile display added in navbar after login

--- src/v1/Components/Navbar/Navbar.js
+++ src/v1/Components/Navbar/Navbar.js
@@ -7,10 +7,16 @@ import Modal from '../Modal';
 import Login from '../Auth/Login';
 import Signup from '../Auth/Signup';
 import { Auth } from '../Auth/Auth';
+import { useSelector } from 'react-redux';
+
+const mapStatetoProps = ({auth})=>({auth})
 
 export const  Navbar = ()=>{
     const [showLogin, setShowLogin] = useState(false)
     const [showSignup, setShowSignup] = useState(false) 
+    const {auth} = useSelector(mapStatetoProps);
+    const isLoggedIn = localStorage.getItem('auth_token')
+    const [openProfile, setOpenProfile] = useState(false);
     return (
         <div>
             <Modal show={showLogin}>
@@ -36,10 +42,35 @@ export const  Navbar = ()=>{
                        <Link to="/contact-us"  className='nav-link'>Contact Us</Link>
                        <Link to="/course-card"  className='nav-link'>Courses</Link>
                 </div>
+                {
+                isLoggedIn?(
+                    <div className="menu-container" >
+                <div
+                  className="navbar-items"
+                  onClick={() => setOpenProfile(!openProfile)}
+                >
+                  <img src="https://api.dicebear.com/5.x/initials/svg?seed=mouli%20chandra" alt="" className='profile-pic' />
+                  <p className="navbar-login">Hey, Hemanth!</p>
+                </div>
+                    {
+                        openProfile &&
+                    <div className="menu-list">
+                    <Link to="/myorders" onClick={() => setOpenProfile(false)} className="menu-item">
+                      <div className="menu-item">Recent Orders</div>
+                    </Link>
+                      <div className="menu-item">
+                        Logout
+                      </div>
+                  </div>
+                    }
+                  </div>
+                ) :(
                 <div className='navbar-auth'>
                     <button className='login-button' onClick={()=>setShowLogin(true)}> Login</button>
                     <button className='signup-button' onClick={()=>setShowSignup(true)}>Sign up</button>
                 </div>
+                )
+                }
 
             </div>
         </div>
